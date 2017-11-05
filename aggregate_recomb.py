8ab67a536ae5221fb92835b972ff504185c84a6f
Kelley <kpaskov@stanford.edu>
2017-11-04 21:44:42 -0700

    split aggregatino scripts

--- aggregate_recomb.py
+++ aggregate_recomb.py
@@ -6,15 +6,15 @@ import numpy as np
 chromosome = sys.argv[1]
 
 # Load data
-family_ids, Xs, Ys, row_indices, col_indices = [], [], [], [], []
+family_ids, Xs, col_indices = [], [], []
 for filename in os.listdir('raw_data'):
     if filename.endswith('%s.npz' % chromosome):
         data = np.load('raw_data/%s' % filename)
         if 'X' in data and 'Y' in data and 'row_indices' in data and 'col_indices' in data:
             family_ids.append(filename.split('.')[0])
             Xs.append(data['X'])
-            Ys.append(data['Y'])
-            row_indices.append(data['row_indices'])
+            #Ys.append(data['Y'])
+            #row_indices.append(data['row_indices'])
             col_indices.append(data['col_indices'])
 
 print('Num families with data:', len(family_ids))
@@ -47,16 +47,3 @@ with open('data/recomb%s.txt' % chromosome, 'w+') as f:
 
 print('Aggregated recombinations.')
 print('Num maternal:', len(maternal_recombinations), 'Num paternal', len(paternal_recombinations))
-
-
-# Aggregate Ys
-fullY = np.zeros((4*len(family_ids), len(variants)), np.int8)
-
-for i, Y in enumerate(Ys):
-    fullY[(4*i):(4*(i+1)), col_indices[i]] = Y
-
-np.savez_compressed('data/Y%s.txt' % chromosome, 
-            Y=fullY, family_ids=family_ids, variant_positions=variants)
-
-print('Aggregated Ys.', fullY.shape)
-
