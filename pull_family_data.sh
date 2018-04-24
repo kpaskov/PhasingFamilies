0cf319f79f3245eedc0c9644044ded5f354374b7
Kelley Paskov <kpaskov@stanford.edu>
2018-04-23 23:58:57 -0700

    updated slurm files for sherlock2

--- pull_family_data.sh
+++ pull_family_data.sh
@@ -4,9 +4,9 @@
 #SBATCH --job-name=pull
 #SBATCH -p dpwall
 #SBATCH -D /scratch/PI/dpwall/DATA/iHART/kpaskov/PhasingFamilies
-#SBATCH -t 20:00:00
-#SBATCH --mem=64G
+#SBATCH -t 30:00:00
+#SBATCH --mem=32G
 
-module load python/3.4.3
+module load py-scipystack/1.0_py36
 
-srun python3 pull_family_data.py ../../vcf/v3.4/$1.reheader.vcf.gz ../../vcf/v3.4/v34.forCompoundHet.ped raw_data $1
+srun python3 pull_family_data.py ../../vcf/v3.4/$1.reheader.vcf.gz ../../vcf/v3.4/v34.forCompoundHet.ped split_gen $1
--- pull_family_data_batch.sh
+++ pull_family_data_batch.sh
@@ -4,14 +4,14 @@
 #SBATCH --job-name=pull
 #SBATCH --output=pull%A_%a.out
 #SBATCH --error=pull%A_%a.err
-#SBATCH --array=1-22
+#SBATCH --array=1-7
 #SBATCH -p dpwall
 #SBATCH -D /scratch/PI/dpwall/DATA/iHART/kpaskov/PhasingFamilies
-#SBATCH -t 20:00:00
-#SBATCH --mem=64G
+#SBATCH -t 30:00:00
+#SBATCH --mem=32G
 
-module load python/3.4.3
+module load py-scipystack/1.0_py36
 
 # Print this sub-job's task ID
 echo "My SLURM_ARRAY_TASK_ID is " $SLURM_ARRAY_TASK_ID
-srun python3 pull_family_data.py ../../vcf/v3.4/$SLURM_ARRAY_TASK_ID.reheader.vcf.gz ../../vcf/v3.4/v34.forCompoundHet.ped raw_data $SLURM_ARRAY_TASK_ID
+srun python3 pull_family_data.py ../../vcf/v3.4/$SLURM_ARRAY_TASK_ID.reheader.vcf.gz ../../vcf/v3.4/v34.forCompoundHet.ped split_gen $SLURM_ARRAY_TASK_ID
