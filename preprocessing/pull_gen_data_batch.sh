538acfd832765b9fd30fabccba902db352c1250b
Kelley Paskov <kpaskov@stanford.edu>
2018-08-03 21:43:21 -0700

    fixing batch script

--- preprocessing/pull_gen_data_batch.sh
+++ preprocessing/pull_gen_data_batch.sh
@@ -14,4 +14,6 @@ module load py-scipystack/1.0_py36
 
 # Print this sub-job's task ID
 echo "My SLURM_ARRAY_TASK_ID is " $SLURM_ARRAY_TASK_ID
-srun python3 phase/pull_gen_data.py ../../vcf/v3.4/$SLURM_ARRAY_TASK_ID.reheader.vcf.gz split_gen_miss $SLURM_ARRAY_TASK_ID
+#srun python3 preprocessing/pull_gen_data.py ../../vcf/v3.4/$SLURM_ARRAY_TASK_ID.reheader.vcf.gz split_gen_miss $SLURM_ARRAY_TASK_ID
+#srun python3 preprocessing/pull_gen_data.py ../../vcf/SSC/ssc.$SLURM_ARRAY_TASK_ID.vcf.gz split_gen_miss_ssc $SLURM_ARRAY_TASK_ID 
+srun python3 preprocessing/pull_gen_data.py ../../../1kg.release.20130502/ALL.chr$SLURM_ARRAY_TASK_ID.phase3_shapeit2_mvncall_integrated_v5.20130502.genotypes.vcf.gz split_gen_miss_1kg $SLURM_ARRAY_TASK_ID
