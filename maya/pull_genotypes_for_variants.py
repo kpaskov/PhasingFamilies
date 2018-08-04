df97c64ba0bb98a5246ef93bf9466319c9f94387
Kelley <kpaskov@stanford.edu>
2018-08-04 14:12:29 -0700

    filter out indels

--- maya/pull_genotypes_for_variants.py
+++ maya/pull_genotypes_for_variants.py
@@ -14,33 +14,38 @@ coordinates = np.asarray([tuple(map(int, x.decode('UTF-8').split('-'))) for x in
 order_indices = np.lexsort((coordinates[:, 1], coordinates[:, 0]))
 reverse_order_indices = np.argsort(order_indices)
 
-#subcoordinates = []
 submatrices = []
 for chrom in range(1, 23):
 	print(chrom)
 	# pull coordinates for chromosome
 	chrom_coords = np.load('%s/chr.%d.gen.coordinates.npy' %(data_dir, chrom))
+
+	# pull only coordinates belonging to snps
+	snp_indices = np.where(chrom_coords[:, 2]==1)[0]
+	chrom_coords = chrom_coords[snp_indices, :]
+
 	to_find = coordinates[order_indices, :]
 	to_find = to_find[to_find[:, 0]==chrom, :]
-	#print(chrom_coords.shape, to_find.shape)
 
-	# find indices of the variants we're looking for on this chromosome
+	# find indices of the variants we're looking for on this chromosome (indices are given in snp_index coordinates)
 	pull_indices = np.searchsorted(chrom_coords[:, 1], to_find[:, 1])
-	#print(np.hstack((to_find, chrom_coords[pull_indices, :])))
 
 	# load genotype data from .npz files
 	offset = 0
-	gen_files = sorted([f for f in listdir(data_dir) if ('chr.%s' % chrom) in f and 'gen.npz' in f])
+	snp_offset = 0
+	gen_files = sorted([f for f in listdir(data_dir) if ('chr.%s.' % chrom) in f and 'gen.npz' in f])
+	print(gen_files)
 	for gen_file in gen_files:
 		part_chrom = sparse.load_npz('%s/%s' % (data_dir, gen_file))
-		m, n = part_chrom.shape
-		part_pull_indices = pull_indices[(pull_indices>=offset) & (pull_indices<(offset+n))]
+		_, n = part_chrom.shape
+		part_chrom = part_chrom[:, snp_indices[(snp_indices>=offset) & (snp_indices < (offset+n))]-offset]
+		_, snp_n = part_chrom.shape
 		
-		#subcoordinates.append(chrom_coords[part_pull_indices, :])
-		submatrices.append(part_chrom[:, part_pull_indices-offset])
+		submatrices.append(part_chrom[:, pull_indices[(pull_indices>=snp_offset) & (pull_indices<(snp_offset+snp_n))]-snp_offset])
+		print(submatrices[-1].shape)
 		offset += n
+		snp_offset += snp_n
 
-#ordered_full_coords = np.vstack(subcoordinates)
 ordered_full_matrix = sparse.hstack(submatrices)
 print(ordered_full_matrix.shape)
 
