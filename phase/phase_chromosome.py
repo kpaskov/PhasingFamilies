1af1567fc2344823fae60d0606150b6a67289339
Kelley <kpaskov@stanford.edu>
2018-09-19 11:06:55 -0700

    lowering mem requirement

--- phase/phase_chromosome.py
+++ phase/phase_chromosome.py
@@ -11,7 +11,7 @@ from scipy import sparse
 import random
 
 
-# Run locally with python3 phase/phase_chromosome.py 22 3 data/160826.ped split_gen phased
+# Run locally with python3 phase/phase_chromosome.py 22 3 data/160826.ped split_gen_miss phased
 
 chrom = sys.argv[1]
 m = int(sys.argv[2])
@@ -165,69 +165,89 @@ print('transitions', transitions.shape)
 pm_gen_to_index = dict()
 pm_gen_indices = []
 for s in inheritance_states:
-	anc_pos = [[-1] if s[i] == 0 else [0, 1] for i in range(4)]
-	anc_variants = np.array(list(product(*anc_pos)), dtype=np.int8)
-	pred_gens = np.zeros((anc_variants.shape[0], m), dtype=np.int8)
+    if s[4] == 0 and s[5] == 0:
+        anc_pos = [[-1] if s[i] == 0 else [0, 1] for i in range(4)]
+        anc_variants = np.array(list(product(*anc_pos)), dtype=np.int8)
+        pred_gens = np.zeros((anc_variants.shape[0], m), dtype=np.int8)
 
-	# mom
-	# deletion
-	pred_gens[(anc_variants[:, 0]==-1) & (anc_variants[:, 1]==-1), 0] = -1
-	pred_gens[(anc_variants[:, 0]==-1) & (anc_variants[:, 1]==0), 0] = 0
-	pred_gens[(anc_variants[:, 0]==-1) & (anc_variants[:, 1]==1), 0] = 2
-	pred_gens[(anc_variants[:, 0]==0) & (anc_variants[:, 1]==-1), 0] = 0
-	pred_gens[(anc_variants[:, 0]==1) & (anc_variants[:, 1]==-1), 0] = 2
-	# normal
-	pred_gens[(anc_variants[:, 0]==0) & (anc_variants[:, 1]==0), 0] = 0
-	pred_gens[(anc_variants[:, 0]==1) & (anc_variants[:, 1]==1), 0] = 2
-	pred_gens[(anc_variants[:, 0]==0) & (anc_variants[:, 1]==1), 0] = 1
-	pred_gens[(anc_variants[:, 0]==1) & (anc_variants[:, 1]==0), 0] = 1
+        # mom
+        # deletion
+        pred_gens[(anc_variants[:, 0]==-1) & (anc_variants[:, 1]==-1), 0] = -1
+        pred_gens[(anc_variants[:, 0]==-1) & (anc_variants[:, 1]==0), 0] = 0
+        pred_gens[(anc_variants[:, 0]==-1) & (anc_variants[:, 1]==1), 0] = 2
+        pred_gens[(anc_variants[:, 0]==0) & (anc_variants[:, 1]==-1), 0] = 0
+        pred_gens[(anc_variants[:, 0]==1) & (anc_variants[:, 1]==-1), 0] = 2
+        # normal
+        pred_gens[(anc_variants[:, 0]==0) & (anc_variants[:, 1]==0), 0] = 0
+        pred_gens[(anc_variants[:, 0]==1) & (anc_variants[:, 1]==1), 0] = 2
+        pred_gens[(anc_variants[:, 0]==0) & (anc_variants[:, 1]==1), 0] = 1
+        pred_gens[(anc_variants[:, 0]==1) & (anc_variants[:, 1]==0), 0] = 1
 
-	# dad
-	# deletion
-	pred_gens[(anc_variants[:, 2]==-1) & (anc_variants[:, 3]==-1), 1] = -1
-	pred_gens[(anc_variants[:, 2]==-1) & (anc_variants[:, 3]==0), 1] = 0
-	pred_gens[(anc_variants[:, 2]==-1) & (anc_variants[:, 3]==1), 1] = 2
-	pred_gens[(anc_variants[:, 2]==0) & (anc_variants[:, 3]==-1), 1] = 0
-	pred_gens[(anc_variants[:, 2]==1) & (anc_variants[:, 3]==-1), 1] = 2
-	# normal
-	pred_gens[(anc_variants[:, 2]==0) & (anc_variants[:, 3]==0), 1] = 0
-	pred_gens[(anc_variants[:, 2]==1) & (anc_variants[:, 3]==1), 1] = 2
-	pred_gens[(anc_variants[:, 2]==0) & (anc_variants[:, 3]==1), 1] = 1
-	pred_gens[(anc_variants[:, 2]==1) & (anc_variants[:, 3]==0), 1] = 1
+        # dad
+        # deletion
+        pred_gens[(anc_variants[:, 2]==-1) & (anc_variants[:, 3]==-1), 1] = -1
+        pred_gens[(anc_variants[:, 2]==-1) & (anc_variants[:, 3]==0), 1] = 0
+        pred_gens[(anc_variants[:, 2]==-1) & (anc_variants[:, 3]==1), 1] = 2
+        pred_gens[(anc_variants[:, 2]==0) & (anc_variants[:, 3]==-1), 1] = 0
+        pred_gens[(anc_variants[:, 2]==1) & (anc_variants[:, 3]==-1), 1] = 2
+        # normal
+        pred_gens[(anc_variants[:, 2]==0) & (anc_variants[:, 3]==0), 1] = 0
+        pred_gens[(anc_variants[:, 2]==1) & (anc_variants[:, 3]==1), 1] = 2
+        pred_gens[(anc_variants[:, 2]==0) & (anc_variants[:, 3]==1), 1] = 1
+        pred_gens[(anc_variants[:, 2]==1) & (anc_variants[:, 3]==0), 1] = 1
 
-	# children
-	for index in range(m-2):
-		mat, pat = s[(4+(2*index)):(6+(2*index))]
-        
-		# deletion
-		pred_gens[(anc_variants[:, mat]==-1) & (anc_variants[:, 2+pat]==-1), 2+index] = -1
-		pred_gens[(anc_variants[:, mat]==-1) & (anc_variants[:, 2+pat]==0), 2+index] = 0
-		pred_gens[(anc_variants[:, mat]==-1) & (anc_variants[:, 2+pat]==1), 2+index] = 2
-		pred_gens[(anc_variants[:, mat]==0) & (anc_variants[:, 2+pat]==-1), 2+index] = 0
-		pred_gens[(anc_variants[:, mat]==1) & (anc_variants[:, 2+pat]==-1), 2+index] = 2
-		# normal
-		pred_gens[(anc_variants[:, mat]==0) & (anc_variants[:, 2+pat]==0), 2+index] = 0
-		pred_gens[(anc_variants[:, mat]==1) & (anc_variants[:, 2+pat]==1), 2+index] = 2
-		pred_gens[(anc_variants[:, mat]==0) & (anc_variants[:, 2+pat]==1), 2+index] = 1
-		pred_gens[(anc_variants[:, mat]==1) & (anc_variants[:, 2+pat]==0), 2+index] = 1
+        # children
+        for index in range(m-2):
+            mat, pat = s[(4+(2*index)):(6+(2*index))]
 
-	unique_pred_gens = set(map(tuple, pred_gens))
-	for pg in unique_pred_gens:
-		if pg not in pm_gen_to_index:
-			pm_gen_to_index[pg] = len(pm_gen_to_index)
-	pm_gen_indices.append([pm_gen_to_index[pg] for pg in unique_pred_gens])
+            # deletion
+            pred_gens[(anc_variants[:, mat]==-1) & (anc_variants[:, 2+pat]==-1), 2+index] = -1
+            pred_gens[(anc_variants[:, mat]==-1) & (anc_variants[:, 2+pat]==0), 2+index] = 0
+            pred_gens[(anc_variants[:, mat]==-1) & (anc_variants[:, 2+pat]==1), 2+index] = 2
+            pred_gens[(anc_variants[:, mat]==0) & (anc_variants[:, 2+pat]==-1), 2+index] = 0
+            pred_gens[(anc_variants[:, mat]==1) & (anc_variants[:, 2+pat]==-1), 2+index] = 2
+            # normal
+            pred_gens[(anc_variants[:, mat]==0) & (anc_variants[:, 2+pat]==0), 2+index] = 0
+            pred_gens[(anc_variants[:, mat]==1) & (anc_variants[:, 2+pat]==1), 2+index] = 2
+            pred_gens[(anc_variants[:, mat]==0) & (anc_variants[:, 2+pat]==1), 2+index] = 1
+            pred_gens[(anc_variants[:, mat]==1) & (anc_variants[:, 2+pat]==0), 2+index] = 1
+
+        unique_pred_gens = set(map(tuple, pred_gens))
+        for pg in unique_pred_gens:
+            if pg not in pm_gen_to_index:
+                pm_gen_to_index[pg] = len(pm_gen_to_index)
+        pm_gen_indices.append([pm_gen_to_index[pg] for pg in unique_pred_gens])
 
 pm_gen = np.zeros((len(pm_gen_to_index), m), dtype=np.int8)
 for pm, i in pm_gen_to_index.items():
 	pm_gen[i, :] = pm
-print('perfect matches', pm_gen.shape, Counter([len(v) for v in pm_gen_indices]))
+print('perfect matches', pm_gen.shape, len(pm_gen_indices), Counter([len(v) for v in pm_gen_indices]))
+
+# losses are symmetrical to parental chromosome swaps
+full_loss_indices = np.zeros((p,), dtype=int)
+loss_state_to_index = dict([(tuple(x), i) for i, x in enumerate(inheritance_states[(inheritance_states[:, 4]==0) & (inheritance_states[:, 5]==0), :])])
+for i, s in enumerate(inheritance_states):
+	new_s = -np.ones((state_len,), dtype=np.int8)
+	if s[4] == 0:
+		new_s[:2] = s[:2]
+		new_s[np.arange(4, state_len, 2)] = s[np.arange(4, state_len, 2)]
+	else:
+		new_s[:2] = s[[1, 0]]
+		new_s[np.arange(4, state_len, 2)] = 1-s[np.arange(4, state_len, 2)]
+	if s[5] == 0:
+		new_s[2:4] = s[2:4]
+		new_s[np.arange(5, state_len, 2)] = s[np.arange(5, state_len, 2)]
+	else:
+		new_s[2:4] = s[[3, 2]]
+		new_s[np.arange(5, state_len, 2)] = 1-s[np.arange(5, state_len, 2)]
+	full_loss_indices[i] = loss_state_to_index[tuple(new_s)]
 
 genotypes = np.array(list(product(*[[0, 1, 2]]*m)), dtype=np.int8)
 genotype_to_index = dict([(tuple(x), i) for i, x in enumerate(genotypes)])
 q = genotypes.shape[0]
 print('genotypes', genotypes.shape)
 
-losses = np.zeros((p, q), dtype=np.int8)
+losses = np.zeros((int(p/4) if m>=5 else p, q), dtype=np.int8)
 already_calculated = np.zeros((q,), dtype=bool)
 def calculate_loss(gen): 
 	gen_index = genotype_to_index[tuple(gen)]
@@ -239,7 +259,7 @@ def calculate_loss(gen):
 		for i, indices in enumerate(pm_gen_indices):
 			losses[i, gen_index] = np.min(s[indices])
 		already_calculated[gen_index] = True
-	return losses[:, gen_index]
+	return losses[full_loss_indices, gen_index]
 
 # pull genotype data from .npz
 indices_of_interest = sum([v for k, v in families_of_this_size], [])
