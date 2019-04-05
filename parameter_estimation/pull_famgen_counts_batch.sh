ae8d0d97a4d91faf2150ada2e3003530ab81cbb6
Kelley Paskov <kpaskov@stanford.edu>
2019-04-05 14:02:26 -0700

    updating batch scripts

--- parameter_estimation/pull_famgen_counts_batch.sh
+++ parameter_estimation/pull_famgen_counts_batch.sh
@@ -7,10 +7,10 @@
 #SBATCH --array=1-22
 #SBATCH -p dpwall
 #SBATCH -D /scratch/PI/dpwall/DATA/iHART/kpaskov/PhasingFamilies
-#SBATCH -t 30:00:00
 #SBATCH --mem=32G
 
-module load py-scipystack/1.0_py36
+module load py-numpy/1.14.3_py36
+module load py-scipy/1.1.0_py36
 
 # Print this sub-job's task ID
 echo "My SLURM_ARRAY_TASK_ID is " $SLURM_ARRAY_TASK_ID
--- phase/phase_chromosome_batch.sh
+++ phase/phase_chromosome_batch.sh
@@ -2,9 +2,9 @@
 #
 #
 #SBATCH --job-name=phase
-#SBATCH --output=phase%A_%a.out
-#SBATCH --error=phase%A_%a.err
-#SBATCH --array=1-22
+#SBATCH --output=logs/phase%A_%a.out
+#SBATCH --error=logs/phase%A_%a.err
+#SBATCH --array=1-1
 #SBATCH -p dpwall
 #SBATCH -D /scratch/PI/dpwall/DATA/iHART/kpaskov/PhasingFamilies
 #SBATCH -t 30:00:00
@@ -14,4 +14,4 @@
 echo "My SLURM_ARRAY_TASK_ID is " $SLURM_ARRAY_TASK_ID
 
 module load py-scipystack/1.0_py36
-srun python3 phase/phase_chromosome.py $SLURM_ARRAY_TASK_ID $1 ../../vcf/v3.4/v34.vcf.ped split_gen phased
+srun python3 phase/phase_chromosome.py $SLURM_ARRAY_TASK_ID $1 ../../vcf/v3.4/v34.vcf.ped split_gen_ihart phased_ihart_stricter phase/float_params.json True $2 $3
