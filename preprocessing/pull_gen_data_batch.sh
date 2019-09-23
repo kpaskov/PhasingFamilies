a5be6b58c14cbcdc01726c614a47204b7073bedc
Kelley Paskov <kpaskov@stanford.edu>
2019-09-23 08:30:19 -0700

    updated sh scripts

--- preprocessing/pull_gen_data_batch.sh
+++ preprocessing/pull_gen_data_batch.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=pull
 #SBATCH --output=logs/pull_%a.out
 #SBATCH --error=logs/pull_%a.err
-#SBATCH --array=1-22
+#SBATCH --array=1-25
 #SBATCH -p dpwall
 #SBATCH -D /oak/stanford/groups/dpwall/users/kpaskov/PhasingFamilies
 #SBATCH -t 30:00:00
@@ -15,7 +15,7 @@ module load py-scipystack/1.0_py36
 # Print this sub-job's task ID
 echo "My SLURM_ARRAY_TASK_ID is " $SLURM_ARRAY_TASK_ID
 
-#srun python3 preprocessing/pull_gen_data.py ../../vcf/v3.4/$SLURM_ARRAY_TASK_ID.reheader.vcf.gz split_gen_miss $SLURM_ARRAY_TASK_ID
+srun python3 preprocessing/pull_gen_data.py ../../../ihart/vcf/v34/$SLURM_ARRAY_TASK_ID.reheader.vcf.gz split_gen_ihart $SLURM_ARRAY_TASK_ID
 
 #srun python3 preprocessing/pull_gen_data.py ../../vcf/SSC/ssc.$SLURM_ARRAY_TASK_ID.vcf.gz split_gen_miss_ssc $SLURM_ARRAY_TASK_ID 
 
@@ -31,4 +31,4 @@ echo "My SLURM_ARRAY_TASK_ID is " $SLURM_ARRAY_TASK_ID
 
 #srun python3 preprocessing/pull_gen_data.py /scratch/groups/dpwall/DATA/iHART/vcf/ssc.hg38.liftover.hg19/phase4/$SLURM_ARRAY_TASK_ID.phase4.HG38toHG19.vcf.gz split_gen_ssc_phase4 $SLURM_ARRAY_TASK_ID
 
-srun python3 preprocessing/pull_gen_data.py /oak/stanford/groups/dpwall/simons_spark/snp/SPARK.30K.array_genotype.20190818.phaseable.passing.vcf.gz split_gen_spark $SLURM_ARRAY_TASK_ID
+#srun python3 preprocessing/pull_gen_data.py /oak/stanford/groups/dpwall/simons_spark/snp/SPARK.30K.array_genotype.20190818.phaseable.passing.vcf.gz split_gen_spark $SLURM_ARRAY_TASK_ID
