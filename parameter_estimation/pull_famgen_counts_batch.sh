65bb31e00842e1db529dc7ec29e26f22a626d480
Kelley Paskov <kpaskov@stanford.edu>
2020-02-13 14:09:57 -0800

    cleaning up batch scripts

--- parameter_estimation/pull_famgen_counts_batch.sh
+++ parameter_estimation/pull_famgen_counts_batch.sh
@@ -15,28 +15,54 @@ module load py-scipy/1.1.0_py36
 # Print this sub-job's task ID
 echo "My SLURM_ARRAY_TASK_ID is " $SLURM_ARRAY_TASK_ID
 
-#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart data/v34.vcf.ped $SLURM_ARRAY_TASK_ID
+# ------------------------ For parameter estimation paper -----------------------
 
-#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart data/v34.vcf.ped $SLURM_ARRAY_TASK_ID --exclude data/btu356-suppl_data/btu356_LCR-hs37d5.bed/btu356_LCR-hs37d5.bed split_gen_ihart_HCR 
+# ihart
+srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart data/v34.vcf.ped $SLURM_ARRAY_TASK_ID split_gen_ihart
 
-#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart data/v34.vcf.ped $SLURM_ARRAY_TASK_ID --include data/btu356-suppl_data/btu356_LCR-hs37d5.bed/btu356_LCR-hs37d5.bed split_gen_ihart_LCR
+# ihart HCR
+srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart data/v34.vcf.ped $SLURM_ARRAY_TASK_ID split_gen_ihart_HCR --exclude data/btu356-suppl_data/btu356_LCR-hs37d5.bed/btu356_LCR-hs37d5.bed
 
-#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark data/spark.ped $SLURM_ARRAY_TASK_ID 
+# ihart LCR
+srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart data/v34.vcf.ped $SLURM_ARRAY_TASK_ID split_gen_ihart_LCR --include data/btu356-suppl_data/btu356_LCR-hs37d5.bed/btu356_LCR-hs37d5.bed
 
-#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ssc data/ssc.ped $SLURM_ARRAY_TASK_ID
+# ihart (GATK 3.2)
+srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart3.2 data/v34.vcf.ped $SLURM_ARRAY_TASK_ID split_gen_ihart3.2
 
-#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart_chip data/v34.vcf.ped $SLURM_ARRAY_TASK_ID
+# ihart EX
+srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart3.2 data/v34.vcf.ped $SLURM_ARRAY_TASK_ID split_gen_ihart_EX --include data/VCRome_2_1_hg19_primary_targets.bed
 
-#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart_famsize data/v34.vcf.ped.famsize.ped $SLURM_ARRAY_TASK_ID  
+# ihart identicals
+srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart_identicals data/ihart_identicals.ped $SLURM_ARRAY_TASK_ID split_gen_ihart_identicals
 
-#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome_pilot /scratch/groups/dpwall/DATA/SSC/SPARK/phenotype/spark.ped $SLURM_ARRAY_TASK_ID
+# spark exome EX
+srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark.ped $SLURM_ARRAY_TASK_ID split_gen_spark_exome_EX --include data/VCRome_2_1_hg38_primary_targets_liftover_ordered.bed 
 
-#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark_jae.ped $SLURM_ARRAY_TASK_ID --include data/exome_calling_regions.v1.interval_list split_gen_spark_exome_EX
+# spark exome EX25
+srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark.ped $SLURM_ARRAY_TASK_ID split_gen_spark_exome_EX25 --include data/VCRome_2_1_hg38_primary_targets_liftover_0_25.bed
 
-#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark_jae.ped $SLURM_ARRAY_TASK_ID --exclude data/btu356-suppl_data/btu356_LCR-hs37d5.bed/btu356_LCR-hs37d5.bed split_gen_spark_exome_HCR
+# spark exome EX50
+srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark.ped $SLURM_ARRAY_TASK_ID split_gen_spark_exome_EX50 --include data/VCRome_2_1_hg38_primary_targets_liftover_25_50.bed
 
-#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark_jae.ped $SLURM_ARRAY_TASK_ID --include data/btu356-suppl_data/btu356_LCR-hs37d5.bed/btu356_LCR-hs37d5.bed split_gen_spark_exome_LCR
+# spark exome EX75
+srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark.ped $SLURM_ARRAY_TASK_ID split_gen_spark_exome_EX75 --include data/VCRome_2_1_hg38_primary_targets_liftover_50_75.bed
 
-#srun python3 preprocessing/pull_famgen_counts.py split_gen_ssc_pilot data/ssc.ped $SLURM_ARRAY_TASK_ID
+# spark exome EX1000
+srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark.ped $SLURM_ARRAY_TASK_ID split_gen_spark_exome_EX1000 --include data/VCRome_2_1_hg38_primary_targets_liftover_75_1000.bed
 
-#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark data/spark.ped $SLURM_ARRAY_TASK_ID
+# spark exome identicals
+srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark_exome_identicals.ped $SLURM_ARRAY_TASK_ID split_gen_spark_exome_EX_identicals --include data/VCRome_2_1_hg38_primary_targets_liftover_ordered.bed
+
+# spark
+srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark data/spark.ped $SLURM_ARRAY_TASK_ID split_gen_spark
+
+# spark identicals
+srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark data/spark_identicals.ped $SLURM_ARRAY_TASK_ID split_gen_spark_identicals
+
+# ------------------------ For deletions paper -----------------------    
+
+# platinum
+#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_platinum data/platinum.ped $SLURM_ARRAY_TASK_ID split_gen_platinum
+
+# SSC
+#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ssc data/ssc.ped $SLURM_ARRAY_TASK_ID split_gen_ssc
--- phase/phase_chromosome_batch.sh
+++ phase/phase_chromosome_batch.sh
@@ -4,10 +4,10 @@
 #SBATCH --job-name=phase
 #SBATCH --output=logs/phase_%a.out
 #SBATCH --error=logs/phase_%a.err
-#SBATCH --array=1-23%15
+#SBATCH --array=1-22%15
 #SBATCH -p dpwall
 #SBATCH -D /oak/stanford/groups/dpwall/users/kpaskov/PhasingFamilies
-#SBATCH -t 30:00:00
+#SBATCH -t 5:00:00
 #SBATCH --mem=64G
 
 # Print this sub-job's task ID
@@ -16,13 +16,19 @@ echo "My SLURM_ARRAY_TASK_ID is " $SLURM_ARRAY_TASK_ID
 module load py-numpy/1.14.3_py36
 module load py-scipy/1.1.0_py36
 
-srun python3 phase/phase_chromosome.py $SLURM_ARRAY_TASK_ID $1 data/v34.vcf.ped split_gen_ihart phased_ihart parameter_estimation/ihart_ind_params.json $2 $3
+#srun python3 phase/phase_chromosome.py $SLURM_ARRAY_TASK_ID $1 data/v34.vcf.ped split_gen_ihart 37 phased_ihart parameter_estimation/ihart_ind_pass_nodel_params.json $2 $3
 
-#srun python3 phase/phase_chromosome.py $SLURM_ARRAY_TASK_ID $1 data/ssc.ped split_gen_ssc phased_ssc parameter_estimation/ssc_params.json $2 $3 
+srun python3 phase/phase_chromosome.py $SLURM_ARRAY_TASK_ID $1 data/platinum.ped split_gen_platinum 37 phased_platinum parameter_estimation/platinum_params.json $2 $3
 
-#srun python3 phase/phase_chromosome.py $SLURM_ARRAY_TASK_ID $1 data/v34.vcf.ped split_gen_ihart_chip phased_ihart_chip parameter_estimation/ihart_chip_ind_params.json $2 $3
+#srun python3 phase/phase_chromosome.py $SLURM_ARRAY_TASK_ID $1 data/ssc.ped split_gen_ssc 37 phased_ssc parameter_estimation/ssc_ind_pass_nodel_params.json $2 $3 
 
-#srun python3 phase/phase_chromosome.py $SLURM_ARRAY_TASK_ID $1 data/spark.ped split_gen_spark phased_spark parameter_estimation/spark_ind_params.json $2 $3
+#srun python3 phase/phase_chromosome.py $SLURM_ARRAY_TASK_ID $1 data/v34.vcf.ped split_gen_ihart_chip phased_ihart_chip parameter_estimation/ihart_chip_ind_pass_params.json $2 $3
+
+#srun python3 phase/phase_chromosome.py $SLURM_ARRAY_TASK_ID $1 data/spark_jae.ped split_gen_spark phased_spark parameter_estimation/spark_ind_pass_params.json $2 $3
+
+#srun python3 phase/phase_chromosome.py $SLURM_ARRAY_TASK_ID $1 /scratch/groups/dpwall/DATA/SSC/SPARK/phenotype/spark.ped split_gen_spark_exome_pilot phased_spark_exome_pilot parameter_estimation/spark_exome_pilot_ind_pass_params.json $2 $3
+
+#srun python3 phase/phase_chromosome.py $SLURM_ARRAY_TASK_ID $1 data/spark2.ped split_gen_spark_exome 38 phased_spark_exome parameter_estimation/spark_exome_ind_pass_nodel_params.json $2 $3
 
 #srun python3 phase/phase_chromosome.py $SLURM_ARRAY_TASK_ID $1 data/v34.vcf.ped split_gen_ihart phased_ihart_males_only parameter_estimation/ihart_params.json $2 $3
 
