c51d75c9dbeb872bbaf6a346c7013be7ec4097ed
Kelley <kpaskov@stanford.edu>
2020-04-23 22:24:33 -0700

    new counts

--- parameter_estimation/pull_famgen_counts_batch.sh
+++ parameter_estimation/pull_famgen_counts_batch.sh
@@ -7,7 +7,8 @@
 #SBATCH --array=1-22%15
 #SBATCH -p dpwall
 #SBATCH -D /oak/stanford/groups/dpwall/users/kpaskov/PhasingFamilies
-#SBATCH --mem=16G
+#SBATCH -t 30:00:00
+#SBATCH --mem=8G
 
 module load py-numpy/1.14.3_py36
 module load py-scipy/1.1.0_py36
@@ -19,6 +20,7 @@ echo "My SLURM_ARRAY_TASK_ID is " $SLURM_ARRAY_TASK_ID
 
 # ihart
 #srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart data/v34.vcf.ped $SLURM_ARRAY_TASK_ID split_gen_ihart
+srun python3 parameter_estimation/pull_famgen_counts_vcf.py /oak/stanford/groups/dpwall/ihart/vcf/v34/$SLURM_ARRAY_TASK_ID.reheader.vcf.gz data/v34.vcf.ped $SLURM_ARRAY_TASK_ID counts_ihart --family_sizes 3 4 5 6 7 --depth_bins 20 30
 
 # ihart HCR
 #srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart data/v34.vcf.ped $SLURM_ARRAY_TASK_ID split_gen_ihart_HCR --exclude data/btu356-suppl_data/btu356_LCR-hs37d5.bed/btu356_LCR-hs37d5.bed
@@ -33,10 +35,11 @@ echo "My SLURM_ARRAY_TASK_ID is " $SLURM_ARRAY_TASK_ID
 # srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart3.2 data/v34.vcf.ped $SLURM_ARRAY_TASK_ID split_gen_ihart_EX --include data/VCRome_2_1_hg19_primary_targets.bed
 
 # ihart identicals
-srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart data/ihart_identicals.ped $SLURM_ARRAY_TASK_ID split_gen_ihart_identicals
+# srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart data/ihart_identicals.ped $SLURM_ARRAY_TASK_ID split_gen_ihart_identicals
 
 # spark exome EX
 # srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark.ped $SLURM_ARRAY_TASK_ID split_gen_spark_exome_EX --include data/VCRome_2_1_hg38_primary_targets_liftover_ordered.bed 
+#srun python3 parameter_estimation/pull_famgen_counts_vcf.py /oak/stanford/groups/dpwall/ihart/vcf/v34/$SLURM_ARRAY_TASK_ID.reheader.vcf.gz data/v34.vcf.ped $SLURM_ARRAY_TASK_ID counts_ihart --family_sizes 3 4 5 6 7 --depth_bins 20 30
 
 # spark exome EX25
 # srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark.ped $SLURM_ARRAY_TASK_ID split_gen_spark_exome_EX25 --include data/VCRome_2_1_hg38_primary_targets_liftover_0_25.bed
