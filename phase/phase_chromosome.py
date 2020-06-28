756608d1f61a5fc7ed2888af354f46fa133cba72
Kelley <kpaskov@stanford.edu>
2020-06-28 12:14:55 -0700

    added consanguinity detection

--- phase/phase_chromosome.py
+++ phase/phase_chromosome.py
@@ -79,7 +79,11 @@ if args.batch_size is not None:
 # if we're modeling parental consanguinity, we can only work with nuclear families 
 if args.detect_consanguinity:
 	families = [x for x in families if x.num_ancestors()==2 and len(x.ordered_couples)==1]
-
+	
+	# to detect consanguinity, model a shared ancestor
+	for family in families:
+		family.add_child(family.mat_ancestors[0], 'shared_ancestor', 'maternal_unshared_ancestor')
+		family.add_child(family.pat_ancestors[0], 'shared_ancestor', 'paternal_unshared_ancestor')
 
 print('Families of interest', len(families))
 
