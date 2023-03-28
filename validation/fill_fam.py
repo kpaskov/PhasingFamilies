8e3af28efae1238e7a26e7c1e4eaaf9abd2e32b0
Kelley <kpaskov@stanford.edu>
2023-03-28 15:27:04 -0400

    fixing fam file

--- validation/fill_fam.py
+++ validation/fill_fam.py
@@ -4,11 +4,15 @@ fam_file = sys.argv[1]
 ped_file = sys.argv[2]
 
 with open(fam_file, 'r') as f:
-	ind_ids = [line.split()[1] for line in f]
+	lines = [line for line in f]
 
 with open(ped_file, 'r') as f:
 	ind_id_to_line = dict([(line.split()[1], ' '.join(line.split())) for line in f])
 
 with open(fam_file, 'w+') as f:
-	for ind_id in ind_ids:
-		f.write(ind_id_to_line[ind_id])
+	for line in lines:
+		ind_id = line.split()[1]
+		if ind_id in ind_id_to_line:
+			f.write(ind_id_to_line[ind_id])
+		else:
+			f.write(line)
